26f905a557f6b092ec3decbafabba8a645319c3e
Axaerus <40496042@live.napier.ac.uk>
2023-03-21 15:58:53 +0000

    WIP harness
    Harness is nearly functionally complete to testing all possible iterations required for tuning

--- src/coursework/ExampleEvolutionaryAlgorithmTest.java
+++ src/coursework/ExampleEvolutionaryAlgorithmTest.java
@@ -10,6 +10,23 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+class SetupConfig {
+    int Population = 0;
+    int HiddenLayers = 0;
+    double MutationChange = 0;
+    double MutationRate = 0;
+
+    double Fitness = 0;
+
+    SetupConfig(int Pop, int HL, double MC, double MR, double Fit) {
+        Population = Pop;
+        HiddenLayers = HL;
+        MutationChange = MC;
+        MutationRate = MR;
+        Fitness = Fit;
+    }
+}
+
 class ExampleEvolutionaryAlgorithmTest {
 
     @org.junit.jupiter.api.Test
@@ -24,18 +41,63 @@ class ExampleEvolutionaryAlgorithmTest {
         Parameters.maxEvaluations = 20000; // Used to terminate the EA after this many generations (does not change)
         assertEquals(20000, Parameters.maxEvaluations);
 
-        int _pop = 20;
-        int _hdlyr = 5;
-        double _mc = 0.1;
-        double _mr = 0.01;
+        int _popMod = 0;
+        int _hdlyrMod = -4;
+        double _mcMod = -0.1;
+        double _mrMod = -0.01;
+        int bitmask = 1;
 
-        while (_pop <= 200)
+        ArrayList<SetupConfig> configs = new ArrayList<SetupConfig>();
+
+        while (bitmask < 16)
         {
-            ParamSetup(_pop, _hdlyr, _mc, _mr); // Default set up!
+            int pop = 20 + (_popMod * (bitmask & 8));
+            int hiddenLayers = 5 + (_hdlyrMod * (bitmask & 4));
+            double mutationChange = 0.1 + (_mcMod * (bitmask & 2));
+            double mutationRate = 0.01 + (_mrMod * (bitmask & 1));
+            ParamSetup(pop, hiddenLayers, mutationChange, mutationRate); // Default set up!
             double fitness = runningOrder(evo);
-            //System.out.println("\n");
-            _pop += 1;
+
+            if ((_popMod >= 180) || (_hdlyrMod >= 0) || (_mcMod >= 0.9) || (_mrMod >= 0.99)) {
+                bitmask += 1;
+                System.out.println("State = " + bitmask);
+            }
+
+            if ((bitmask & 8) == 1) {
+                _popMod += 1;
+            }
+            else {
+                _popMod = 0;
+            }
+            if ((bitmask & 4) == 1) {
+                _hdlyrMod += 1;
+            }
+            else {
+                _hdlyrMod = -4;
+            }
+            if ((bitmask & 2) == 1) {
+                _mcMod += 0.01;
+            }
+            else {
+                _mcMod = 0;
+            }
+            if ((bitmask & 1) == 1) {
+                _mrMod += 0.01;
+            }
+            else {
+                _mrMod = 0;
+            }
+
+            configs.add(new SetupConfig(pop, hiddenLayers, mutationChange, mutationRate, fitness));
         }
+
+        Collections.sort(configs, new Comparator<SetupConfig>() {
+            @Override
+            public int compare(SetupConfig o1, SetupConfig o2) {
+                return o1.Fitness < o2.Fitness ? 1 : 0;
+            }
+        });
+        System.out.println("Best fitness found = " + configs.get(0).Fitness);
     }
 
     void ParamSetup(int PopulationSize, int HiddenLayers, double MutationChange, double MutationRate) {
